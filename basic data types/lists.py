105aed0edc0c0987ec7c7a53557214a936293f4b
Ferhat Vurucu <ferhatvrcu@gmail.com>
2020-08-25 19:12:47 +0300

    minor updates

--- basic data types/lists.py
+++ basic data types/lists.py
@@ -12,9 +12,9 @@ Consider a list (list = []). You can perform the following commands:
 Initialize your list and read in the value of n followed by n lines of commands where each command will be of the 7 types listed above. 
 Iterate through each command in order and perform the corresponding operation on your list.
 '''
-N = int(input())
+n = int(input())
 arr = []
-for i in range(N):
+for i in range(n):
     command, *values = input().split()
     if command == "print":
         print(arr)
